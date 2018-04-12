a6a5916d098fbe334d2115fc40db43bffbf72dfe
github username <github email address>
2018-04-12 16:59:52 -0400

    created user follower and following relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password_digest:              "foobar")
+
+99.times do |n|
+  name  = "John Smith#{n+1}"
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password_digest:              password)
+end
+
+# Microposts
+users = User.order(:created_at).take(6)
+50.times do
+  content = "Hello World"
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
